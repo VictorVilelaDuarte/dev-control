62e4179df9f17398ba691e154202be99947753c1
VictorVilelaDuarte <victorvduarte@gmail.com>
2025-09-22 23:40:19 -0300

    creating homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import HeroImg from "@/assets/hero.svg";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to Dev - control</h1>
+    <main className="flex flex-col items-center justify-center min-h-[calc(100vh-80px)]">
+      <h2 className="font-medium text-2xl mb-2">Gerencie seus chamados</h2>
+      <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">
+        Atendimentos, clientes
+      </h1>
+
+      <Image
+        src={HeroImg}
+        alt="Imagem hero da home"
+        width={600}
+        className="max-w-sm md:max-w-xl"
+      />
     </main>
   );
 }
